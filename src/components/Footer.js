b23118bfb7796ee725d8dee6de5a53f58adaed68
2126ReshmaB <resh21331.cs@rmkec.ac.in>
2024-10-15 11:34:51 +0530

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Layout from "./Layout";
-import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg font-mono dark:text-light dark:border-light sm:text-base">
       <Layout className="py-8 flex items-center justify-center lg:flex-col lg:py-6">
-        <span>"The beautiful thing about learning is that no one can take it away from you." - B.B. King</span>
-        
+        <span>&quot;The beautiful thing about learning is that no one can take it away from you.&quot; - B.B. King</span>
       </Layout>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,30 +10,6 @@ import Experience from '@/components/Experience';
 import Education from '@/components/Education';
 import TransitionEffect from '@/components/TransitionEffect';
 
-// const AnimatedNumbers = ({ value }) => {
-//   const ref = useRef(null);
-
-//   const motionValue = useMotionValue(0);
-//   const springValue = useSpring(motionValue, { duration: 3000 })
-//   const isInView = useInView(ref, { once: true });
-
-//   useEffect(() => {
-//     if (isInView) {
-//       motionValue.set(value);
-//     }
-//   }, [isInView, value, motionValue])
-
-//   useEffect(() => {
-//     springValue.on("change", (latest) => {
-//       if (ref.current && latest.toFixed(0) <= value) {
-//         ref.current.textContent = latest.toFixed(0);
-//       }
-//     })
-//   }, [springValue, value])
-
-
-//   return <span ref={ref}></span>
-// }
 const about = () => {
   return (
     <>
@@ -44,57 +20,31 @@ const about = () => {
       <TransitionEffect />
       <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className="pt-16">
-          {/* <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' /> */}
           <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
             <div className='col-span-5 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
-              <h2 className='mb-4 text-lg font-bold uppercse text-dark/7 dark:text-light/75 font-mono'>Biography</h2>
+              <h2 className='mb-4 text-lg font-bold uppercase text-dark/7 dark:text-light/75 font-mono'>Biography</h2>
               
               <p className='font-mono font-medium mt-12'>
-              Hi, I'm Reshma B, a Final-year Computer Science student at RMK Engineering College. I started with a biology background in high school but transitioned to computer science due to my strong interest in the field.
+              Hi, I&apos;m Reshma B, a Final-year Computer Science student at RMK Engineering College. I started with a biology background in high school but transitioned to computer science due to my strong interest in the field.
 
               Receiving a scholarship from the Foundation For Excellence (FFE) has been life-changing. It has not only provided financial support but also opened doors to valuable mentorship and specialized classes beyond my college curriculum. These opportunities have significantly enhanced my technical and professional growth.
 
-Since my first year, I've been involved in practical projects, especially in Data Structures and Algorithms, building a solid foundation in these areas.
+Since my first year, I&apos;ve been involved in practical projects, especially in Data Structures and Algorithms, building a solid foundation in these areas.
 
-I believe that true passion for our work goes beyond aesthetics; it's about genuine interest and continuous learning.
+I believe that true passion for our work goes beyond aesthetics; it&apos;s about genuine interest and continuous learning.
               </p>
-
-              
-              
             </div>
             <div className='flex justify-end col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4  md:order-1 md:col-span-8'>
               <div className='flex justify-end absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
               <Image src={profilePic} alt="Codebucks" className='w-full h-auto rounded-2xl' priority size="(max-width: 768px) 100vw,(max-width: 1200px) 50vw, 33vw" />
             </div>
-             {/* <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
-              <div className='flex flex-col items-end justify-center  xl:items-center'>
-                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={50} />+
-                </span>
-                <h1 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Satisfied cients</h1>
-              </div>
-              <div className='flex flex-col items-end justify-center xl:items-center'>
-                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={40} />+
-                </span>
-                <h1 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Projects Complete</h1>
-              </div>
-              <div className='flex flex-col items-end justify-center xl:items-center'>
-                <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                  <AnimatedNumbers value={4} />+
-                </span>
-                <h1 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Years of experience</h1>
-              </div>
-            </div> */}
           </div>
-          {/* <Skills /> */}
           <Education />
           <Experience />
-          
         </Layout>
       </main>
     </>
   )
 }
 
-export default about
+export default about;
